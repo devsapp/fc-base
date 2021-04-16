ee9eec9796e95d89677bd5ac521a1847adc1690b
千风 <qianfeng.zqf@alibaba-inc.com>
2021-04-16 15:12:31 +0800

    fix(fix report component): fix report component

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ export default class FcBaseComponent {
       uid = credentials.AccountID;
     }
 
-    core.reportComponent(command, {
-      command: componentName,
+    core.reportComponent(componentName, {
+      command,
       uid,
     });
   }
