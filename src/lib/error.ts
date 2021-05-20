55fc81181c417ed6075c0a61d94668c0e24c4748
千风 <qianfeng.zqf@alibaba-inc.com>
2021-05-20 12:00:54 +0800

    perf: add some error handler
    fix import trigger repeatedly error;add SET_VPC_ID_ERROR handler

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -2,6 +2,8 @@ const RESOURCE_ALREADY_EXIST_ERR_MSG = 'already exists';
 const DIAGNOSTICS = 'Diagnostics';
 const REQUESTID = 'RequestId';
 const DURATION = 'Duration';
+const SET_VPC_ID_ERROR = 'has a problem: "vpc_config.0.vpc_id": this field cannot be set';
+
 
 export function handlerKnownErrors(e: Error) {
   const { message } = e;
@@ -10,16 +12,22 @@ export function handlerKnownErrors(e: Error) {
     handlerReourceAlreadyExistErr(message);
   } else if (message.includes(REQUESTID)) {
     handlerUnkonwnErrWithRequestID(message);
+  } else if (message.includes(SET_VPC_ID_ERROR)) {
+    handlerSetVpcIDError();
   }
 }
 
-function handlerUnkonwnErrWithRequestID(errMsg: string) {
+function handlerSetVpcIDError(): void {
+  throw new Error('please delete vpcConfig/vpcId under the service');
+}
+
+function handlerUnkonwnErrWithRequestID(errMsg: string): void {
   const startIdx: number = errMsg.indexOf(DIAGNOSTICS);
   const endIdx: number = errMsg.indexOf(DURATION);
   throw new Error(errMsg.substring(startIdx, endIdx));
 }
 
-function handlerReourceAlreadyExistErr(errMsg: string) {
+function handlerReourceAlreadyExistErr(errMsg: string): void {
   const re = /"ErrorMessage":.*$/gm;
   const errMsgInDiagnostics: string = errMsg.match(re)[0];
   // 去除 "ErrorMessage": 开头，并删除首位双引号
